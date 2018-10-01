3a9fc7c8505ff7e19887492b373ca5ae36af6f54
Luiz Ximenes <ximenes130@gmail.com>
2018-10-01 17:55:01 -0300

    Updating settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_25_125650) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guests", force: :cascade do |t|
     t.string "name"
     t.boolean "confirmed", default: false
